9f0d869d71697c57262b9fecf06ae738f73b523a
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2017-11-03 12:16:30 -0600

    new hw assgn

--- src/App.js
+++ src/App.js
@@ -1,47 +1,20 @@
 import React, { Component } from 'react';
-
-import UserOutput from './User/UserOutput';
-import UserInput from './User/UserInput';
-
 import './App.css';
-import 'bulma/css/bulma.css';
 
 class App extends Component {
-  state = {
-    username: 'Rafael'
-  }
-
-  handleChangeUsername = event => {
-    this.setState(
-      {
-        username: event.target.value
-      }
-    );
-  }
-
   render() {
     return (
-      <section className="App container is-full">
-        <div className="columns">
-          <div className="column">
-            <section className="hero is-primary">
-              <div class="hero-body">
-                <div className="Container">
-                  <h1 className="title">
-                    <UserOutput username={this.state.username}/>
-                  </h1>
-                  <h2 className="subtitle">
-                    <UserInput
-                      change={this.handleChangeUsername}
-                      input={this.state.username}
-                    />
-                  </h2>
-                </div>
-              </div>
-            </section>
-          </div>
-        </div>
-      </section>
+      <div className="App">
+        <ol>
+          <li>Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
+          <li>Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
+          <li>Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)</li>
+          <li>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
+          <li>Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
+          <li>When you click a CharComponent, it should be removed from the entered text.</li>
+        </ol>
+        <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
+      </div>
     );
   }
 }
